bd5eeed5df8d04ee4455953bb833645898e71ce6
Yadira <ysoto@rockefeller.edu>
2017-02-04 11:43:17 -0500

    feature: took untested advice

--- count_seq.sh
+++ count_seq.sh
@@ -1,10 +1,4 @@
-grep -c ">" ~/Desktop/data-shell/data/genomes.fa
-ls ~/Desktop/data-shell/Desktop/data/genome.fa
-git status 
-git add count_seq.sh
-git status
-git commit -m "feat: draft seq counter"
-git status
-nano count_seq.sh
-history
+#!/bin/bash
+# USAGE: bash count_seq.sh FASTA
+grep -c ">" #$1
  
